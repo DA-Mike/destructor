6b25ac0d9ff0e3bb278ffd65eb426f9f333be5bf
Mike Rugh <mike.rugh@gmail.com>
2023-01-12 21:07:49 -0800

    fixes formatting

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from tkinter import filedialog
 from utils.destructor import Destructor as d
 from PIL import ImageTk, Image
 
+
 class App:
+    """This is the application and GUI"""
 
     def __init__(self):
         self.window = tk.Tk()
-        self.ph1 = tk.PhotoImage(file=r'C:\\Users\\t3k_p\\Documents\\projects\\destructor\\destructor\\images\\explosion.png')
+        self.ph1 = tk.PhotoImage(
+            file=r"C:\\Users\\t3k_p\\Documents\\projects\\destructor\\destructor\\images\\explosion.png"
+        )
         self.window.iconphoto(False, self.ph1)
         self.window.title("DESTRUCTOR")
         self.window.geometry("720x625")
@@ -18,69 +22,118 @@ class App:
         self.window.columnconfigure(0, weight=1)
         self.window.rowconfigure(0, weight=1)
 
-        self.elmo = Image.open('./images/elmo.jpg')
-        self.elmo = self.elmo.resize((300,150))
+        self.elmo = Image.open("./images/elmo.jpg")
+        self.elmo = self.elmo.resize((300, 150))
         self.elmo_obj = ImageTk.PhotoImage(self.elmo)
 
-        ttk.Label(self.mainframe, text="Welcome to Destructor", padding='5', font=('Arial', 25), justify='center').grid(column=2, row=1)
-        ttk.Label(self.mainframe, image=self.elmo_obj, padding='5', justify='center', background='black').grid(column=2, row=2, pady=10)
-        ttk.Label(self.mainframe, text="Enter Starting Directory", padding='5').grid(column=1, row=3, sticky=tk.E)
+        ttk.Label(
+            self.mainframe,
+            text="Welcome to Destructor",
+            padding="5",
+            font=("Arial", 25),
+            justify="center",
+        ).grid(column=2, row=1)
+        ttk.Label(
+            self.mainframe,
+            image=self.elmo_obj,
+            padding="5",
+            justify="center",
+            background="black",
+        ).grid(column=2, row=2, pady=10)
+        ttk.Label(self.mainframe, text="Enter Starting Directory", padding="5").grid(
+            column=1, row=3, sticky=tk.E
+        )
 
-        self.folderPath = tk.StringVar()
+        self.folder_path = tk.StringVar()
         self.path = tk.StringVar()
-        self.path_entry = tk.Entry(self.mainframe,textvariable=self.folderPath, justify='center')
+        self.path_entry = tk.Entry(
+            self.mainframe, textvariable=self.folder_path, justify="center"
+        )
         self.path_entry.grid(column=2, row=3, pady=10, sticky=tk.EW)
         self.path_entry.focus()
 
         self.frame1 = ttk.Frame(self.mainframe)
         self.frame1.grid(column=2, row=4, sticky=tk.NSEW, pady=10)
-        self.seek = ttk.Button(self.frame1, text="Seek", padding=5, command=lambda: self.seekHandler()).grid(column=1, row=1, sticky=tk.W, padx=100)
-        self.destroy = ttk.Button(self.frame1, text="Destroy", padding=5, command=lambda: self.verify()).grid(column=4, row=1, stick=tk.E, padx=50)
+        self.seek = ttk.Button(
+            self.frame1, text="Seek", padding=5, command=lambda: self.seek_handler()
+        ).grid(column=1, row=1, sticky=tk.W, padx=100)
+        self.destroy = ttk.Button(
+            self.frame1, text="Destroy", padding=5, command=lambda: self.verify()
+        ).grid(column=4, row=1, stick=tk.E, padx=50)
 
-        self.btnFind = ttk.Button(self.mainframe, text="Browse",command=lambda: self.getFolderPath())
-        self.btnFind.grid(row=3,column=4, padx=10)
+        self.btn_find = ttk.Button(
+            self.mainframe, text="Browse", command=lambda: self.get_folder_path()
+        )
+        self.btn_find.grid(row=3, column=4, padx=10)
 
         self.msg_frame = ttk.Frame(self.mainframe)
         self.msg_frame.grid(column=2, row=5, sticky=tk.NSEW)
         self.msg_frame.config(height=200, width=500)
-        self.msg_box = tk.Text(self.msg_frame, background='white', width=60, height=15, relief='solid', borderwidth=1, state='disabled', fg='black')
+        self.msg_box = tk.Text(
+            self.msg_frame,
+            background="white",
+            width=60,
+            height=15,
+            relief="solid",
+            borderwidth=1,
+            state="disabled",
+            fg="black",
+        )
         self.msg_box.grid(column=2, row=2, sticky=tk.W, pady=30)
 
         self.window.mainloop()
 
     def choice(self, option):
+        """Handles destroy/deletion of node_modules in selected path
+        after user selects 'yes' from verification modal"""
+
         pop.destroy()
         if option == "yes":
-                result = d.destroy(self, self.folderPath.get())
-                self.msg_box.config(state='normal')
-                self.msg_box.insert(tk.INSERT, '\n' + result[0] + '\n' + result[1])
-                self.msg_box.config(state='disabled')
+            result = d.destroy(self, self.folder_path.get())
+            self.msg_box.config(state="normal")
+            self.msg_box.insert(tk.INSERT, "\n" + result[0] + "\n" + result[1])
+            self.msg_box.config(state="disabled")
 
-    def seekHandler(self):
-        result = d.seek(self, self.folderPath.get())
-        self.msg_box.config(state='normal')
-        self.msg_box.insert(tk.INSERT, '\n' + result[0] + '\n' + result[1])
-        self.msg_box.config(state='disabled')
+    def seek_handler(self):
+        """Initiates seek function, insterts results into text module"""
+
+        result = d.seek(self, self.folder_path.get())
+        self.msg_box.config(state="normal")
+        self.msg_box.insert(tk.INSERT, "\n" + result[0] + "\n" + result[1])
+        self.msg_box.config(state="disabled")
 
     def verify(self):
-        global pop 
+        """Validates that the user wants to proceed with
+        deleting node_modules directories"""
+
+        global pop
         pop = tk.Toplevel(self.window)
         pop.geometry("300x300")
-        pop.config(bg='white')
-        label = tk.Label(pop, text="Are you sure? This will permanently delete all node_modules directories in this path.",
-            font=("Arial", 10), wraplength=200)
+        pop.config(bg="white")
+        label = tk.Label(
+            pop,
+            text="Are you sure? This will permanently delete all node_modules directories in this path.",
+            font=("Arial", 10),
+            wraplength=200,
+        )
         label.pack(pady=20)
         frame = tk.Frame(pop)
         frame.pack(pady=10)
-        ok_button = tk.Button(frame, text="Yes", command=lambda: self.choice("yes"), fg="black")
+        ok_button = tk.Button(
+            frame, text="Yes", command=lambda: self.choice("yes"), fg="black"
+        )
         ok_button.grid(row=2, column=1, padx=10)
         ok_button.config(padx=10)
-        cancel_button = tk.Button(frame, text="No", command=lambda: self.choice("no"), fg="black")
+        cancel_button = tk.Button(
+            frame, text="No", command=lambda: self.choice("no"), fg="black"
+        )
         cancel_button.grid(row=2, column=2, padx=10)
         cancel_button.config(padx=10)
 
-    def getFolderPath(self):
+    def get_folder_path(self):
+        """Gets directory path from user"""
         folder_selected = filedialog.askdirectory()
-        self.folderPath.set(folder_selected)
-    
+        self.folder_path.set(folder_selected)
+
+
 App()
--- utils/destructor.py
+++ utils/destructor.py
@@ -2,14 +2,21 @@ import os
 import shutil
 import time
 
+
 class Destructor:
 
-    def __init__(self):
-        self.__init__()
+    """This class has two main functions: Seek and Destroy all node_modules
+    directories in the desired path"""
+
+    # def __init__(self):
+    #     """Init function"""
+    #     self.__init__()
 
     def destroy(self, path):
+        """This walks through the path and deletes node_modules directories.
+        Returns array."""
         count = 0
-        st = time.time()
+        start_time = time.time()
         try:
 
             for dir, subdirs, files in os.walk(path):
@@ -20,32 +27,43 @@ class Destructor:
                 elif subdirs:
                     if len(subdirs) > 1:
                         for sub in subdirs:
-                            if os.path.isdir(os.path.join(dir, sub)) and sub == "node_modules":
+                            if (
+                                os.path.isdir(os.path.join(dir, sub))
+                                and sub == "node_modules"
+                            ):
                                 # remove directory and all its content
                                 shutil.rmtree(os.path.join(dir, sub))
                                 count += 1
                     else:
                         # verify path exists and it's node_modules
-                        if os.path.isdir(os.path.join(dir, subdirs[0])) and subdirs[0] == "node_modules":
+                        if (
+                            os.path.isdir(os.path.join(dir, subdirs[0]))
+                            and subdirs[0] == "node_modules"
+                        ):
                             # remove directory and all its content
                             shutil.rmtree(os.path.join(dir, subdirs[0]))
                             count += 1
                 elif len(files) > 0 or len(files) == 0:
                     pass
                 else:
-                    raise ValueError("Path {} is not a file or dir.".format(path))
-            et = time.time()
-            elapsed_time = et - st
-            d_msg = "{} node_modules directories were destroyed!".format(count)
-            time_msg = "Destroy() took {} seconds to execute".format(elapsed_time)
+                    raise ValueError(f"Path {path} is not a file or dir.")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            d_msg = f"{count} node_modules directories were destroyed!"
+            time_msg = f"Destroy() took {elapsed_time} seconds to execute"
             return [d_msg, time_msg]
 
-        except:
-            return ['Error: ', 'An unknown error occurred.']
+        except ValueError as err:
+            return ["Error: ", err]
+        except OSError as err:
+            return ["Error: ", err]
 
     def seek(self, path):
+        """Seeks out, counts, and returns the number of node_modules
+        directories it finds in an array"""
+
         count = 0
-        st = time.time()
+        start_time = time.time()
         try:
 
             for dir, subdirs, files in os.walk(path):
@@ -54,23 +72,31 @@ class Destructor:
                 elif subdirs:
                     if len(subdirs) > 1:
                         for sub in subdirs:
-                            if os.path.isdir(os.path.join(dir, sub)) and sub == "node_modules":
+                            if (
+                                os.path.isdir(os.path.join(dir, sub))
+                                and sub == "node_modules"
+                            ):
                                 count += 1
                                 subdirs.remove(sub)
                     else:
                         # verify path exists and it's node_modules
-                        if os.path.isdir(os.path.join(dir, subdirs[0])) and subdirs[0] == "node_modules":
+                        if (
+                            os.path.isdir(os.path.join(dir, subdirs[0]))
+                            and subdirs[0] == "node_modules"
+                        ):
                             count += 1
                             subdirs.remove(subdirs[0])
                 elif len(files) > 0 or len(files) == 0:
                     pass
                 else:
-                    raise ValueError("Path {} is not a file or dir.".format(path))
-            et = time.time()
-            elapsed_time = et - st
-            s_msg = "There are {} node_modules directories set for destruction.".format(count)
-            time_msg = "Seek() took {} seconds to execute".format(elapsed_time)
+                    raise ValueError(f"Path {path} is not a file or dir.")
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            s_msg = f"There are {count} node_modules directories set for destruction."
+            time_msg = f"Seek() took {elapsed_time} seconds to execute"
             return [s_msg, time_msg]
-            
-        except:
-            return ['Error: ', 'An unknown error occurred.']
+
+        except ValueError as err:
+            return ["Error: ", err]
+        except OSError as err:
+            return ["Error: ", err]
